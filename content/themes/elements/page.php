79ca2eda21340a51c5986d59b99bff14fa810e92
Luc Awater <lucawater@gmail.com>
2016-03-02 11:43:05 +0100

    Remove parallax elements

--- content/themes/elements/page.php
+++ content/themes/elements/page.php
@@ -9,25 +9,16 @@ if( is_woocommerce() ){
   the_content();
 }
 
-if( is_cart() ){
-  echo '<h2>Cart</h2>';
-}
-
 /*
  * Start the ACF page elements loop
  *
  * Important: acf.json must be imported in order to use elements theme from this point forward.
  */
-if( have_rows('page') ):
-  // Every parallax element needs an id
-  $i_par = 0;
-
+if( have_rows('elements') ):
   // Loop through ACF page elements
-  while( have_rows('page') ): the_row();
+  while( have_rows('elements') ): the_row();
 
-    if( get_row_layout() == 'hero' ):
-      get_template_part( 'elements/hero' );
-    elseif( get_row_layout() == 'text' ):
+    if( get_row_layout() == 'text' ):
       get_template_part( 'elements/text' );
     elseif( get_row_layout() == 'image' ):
       get_template_part( 'elements/image' );
@@ -37,8 +28,6 @@ if( have_rows('page') ):
       get_template_part( 'elements/gridSec' );
     elseif( get_row_layout() == 'slider' ):
       get_template_part( 'elements/slider' );
-    elseif( get_row_layout() == 'parallax' ): $i_par++;
-      get_template_part( 'elements/parallax' );
     endif;
 
   endwhile;
