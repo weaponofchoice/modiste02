00529b8f43db8d95ac220e6e330a9601c44faec3
Luc Awater <lucawater@gmail.com>
2016-03-09 14:45:12 +0100

    Tweak element styles

--- content/themes/elements/elements/module.php
+++ content/themes/elements/elements/module.php
@@ -4,13 +4,21 @@ $b_image = get_sub_field( 'module_b_image' );
 $b_image_url = $b_image['sizes']['medium'];
 $b_image_width = $b_image['sizes']['medium-width'];
 $b_image_height = $b_image['sizes']['medium-height'];
+
+$b_caption = get_sub_field( 'module_b_captionSide' );
+
 $b_text = wpautop( get_sub_field( 'module_b_text' ) );
+
+$b_button = get_sub_field( 'module_b_button' );
+$b_button_text = get_sub_field( 'module_b_button_text' );
+$b_button_link = get_sub_field( 'module_b_button_link' );
 
 // Options
 $o_orientation = get_sub_field( 'module_o_orientation' );
+$o_align = get_sub_field( 'module_o_align' );
 
 // Classes
-$class_section = 'module orientated-' . $o_orientation;
+$class_section = 'module orientated-' . $o_orientation . ' align-' . $o_align;
 $class_body = 'section-body';
 
 // Build section
@@ -23,8 +31,18 @@ section_start( $class_section );
      * Body content
      * This is the flexible part, that is different for each element
      */
-    echo '<img src="' . $b_image_url . '" width="' . $b_image_width . '" height="' . $b_image_height . '">';
-    echo '<div>' . $b_text . '</div>';
+    echo '<p class="is-bold">' . $b_caption . '</p>';
+
+    echo '<div class="module-image"><img src="' . $b_image_url . '" width="' . $b_image_width . '" height="' . $b_image_height . '"></div>';
+
+    echo '<div class="module-text">';
+
+      echo $b_text;
+
+      if( $b_button )
+        echo '<a class="button" href="' . $b_button_link . '">' . $b_button_text . '</a>';
+
+    echo '</div>';
 
   section_body_end();
 
--- content/themes/elements/single.php
+++ content/themes/elements/single.php
@@ -4,10 +4,8 @@ get_header();
 if( have_posts() ):
   while( have_posts() ): the_post();
     ?>
-    
-    <article>
-      <h1><?php the_title(); ?></h1>
 
+    <article>
       <?php get_elements(); ?>
     </article>
 
