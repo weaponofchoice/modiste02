60615b08205fd7201b7f9fbbe08c707269881026
Luc Awater <lucawater@gmail.com>
2016-03-10 16:14:37 +0100

    Improve woocommerce page check

--- content/themes/elements/page.php
+++ content/themes/elements/page.php
@@ -5,9 +5,15 @@ get_header();
  * Check if we are on a WooCommerce page
  * If so, echo content to enable the shortcode
  */
-if( is_woocommerce() ){
-  the_content();
-}
+if( have_posts() ):
+  while( have_posts() ): the_post();
+
+    if( is_really_woocommerce_page() ){
+      the_content();
+    }
+
+  endwhile;
+endif;
 
 get_elements();
 
