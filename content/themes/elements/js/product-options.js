ee20392c2f9bd936d2e4b0c0fbd1ad6a6959fa78
Luc Awater <lucawater@gmail.com>
2016-03-30 15:56:50 +0200

    Fallback for product option placeholder

--- content/themes/elements/js/product-options.js
+++ content/themes/elements/js/product-options.js
@@ -6,7 +6,13 @@ $(document).ready( function() {
 
   for(a = 0; a < options.length; a++){
     var defaultValue = $(options[a]).find('input[checked="checked"]')[0].value;
-    $(options[a]).prepend('<div class="option-placeholder">' + defaultValue + '</div>');
+
+    if( defaultValue ){
+      $(options[a]).prepend('<div class="option-placeholder">' + defaultValue + '</div>');
+    } else {
+      $(options[a]).prepend('<div class="option-placeholder">Choose option</div>');
+    }
+
   }
 
   var label = $('label');
