73468c13c9b223965ea1d41c31740316f971b608
Luc Awater <lucawater@gmail.com>
2016-06-07 14:26:39 +0200

    Project new style test

--- content/themes/elements/content-post.php
+++ content/themes/elements/content-post.php
@@ -7,13 +7,17 @@ $content = wpautop( get_the_content() );
 $permalink = get_the_permalink();
 $date = get_the_date();
 $thumb = get_the_post_thumbnail( $post->ID, 'medium' );
+$image = get_the_post_thumbnail( $post->ID, 'large' );
 ?>
 
 <li>
-  <div>
-    <p><?php echo $date; ?></p>
-    <a class="post-title" href="<?php echo $permalink; ?>"><h1><?php echo $title; ?></h1></a>
-  </div>
+  <figure>
+    <?php echo $image; ?>
+  </figure>
 
-  <a href="<?php echo $permalink; ?>"><?php echo $thumb; ?></a>
+  <div>
+    <h2><?php echo $title; ?></h2>
+
+    <a class="button button-white" href="<?php echo $permalink; ?>">view project</a>
+  </div>
 </li>
