cb42715e6dbb8cca7a69df00852a2dc913fda5b9
Luc Awater <lucawater@gmail.com>
2016-06-29 10:59:28 +0200

    Add Google Analytics tracking ID

--- content/themes/elements/functions.php
+++ content/themes/elements/functions.php
@@ -81,6 +81,21 @@ add_filter( 'woocommerce_show_page_title', function() { return false; } );
 require_once('includes/mobile-detect.php');
 $detect = new Mobile_Detect;
 
+// Add Google Analytics tracking code to footer
+add_action('wp_footer', 'add_googleanalytics');
+function add_googleanalytics() { ?>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-62289835-10', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+<?php }
+
 // Hide admin bar
 add_filter('show_admin_bar', '__return_false');
 
